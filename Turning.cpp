829224439a9b5524d158ec938f5d3145c4bf6a23
MulloFrank <38895960+MulloFrank@users.noreply.github.com>
2018-05-07 10:54:18 +1200

    Add files via upload

---
+++ Turning.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+int main() {
+
+	init();
+	parameter = 0;
+	
+	if (something < 0){
+		parameter = 1;
+	}
+	else if (something > 0){
+		parameter = 2:
+	}
+	else {
+		something == 0;
+	}
+	
+	if (parameter == 1){
+		set_motor(1,255);
+		set_motor(2,-255);
+	}
+	else if ( parameter == 2){
+		set_motor(1,-255);
+		set_motor(2,255);
+	}
+	else if (parameter == 0){
+		set_motor(1,255):
+		set motor(2,255);
+	}
+	
+return0;}
