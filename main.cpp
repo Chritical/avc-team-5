4112f32efa38a23f86982b0d6669ec17c2d8733d
Michael Behan <37396948+HalfSquare@users.noreply.github.com>
2018-05-14 11:12:38 +1200

    Gate open method

--- main.cpp
+++ main.cpp
@@ -58,8 +58,23 @@ void right_motor(int speed)
 {
     set_motor(1, speed);
 }
-rowPixels[i] = 
+
 // Returns dv
+
+int gateOpen(){
+	char ip[15] = "130.195.6.196";
+	while(connect_to_server(ip, 1024)!=0){continue;}
+	printf("Connected");
+	char message[24] = "Please";
+	while(send_to_server(message) != 0){continue;}
+	printf("Message sent");
+	char password[24] = "";
+	while(receive_from_server(password) != 0){continue;}
+	while(send_to_server(password) != 0){continue;}
+	printf("Message sent 2");
+	return 0;
+} 
+
 int read_white_line()
 {
     // Initialise variables
