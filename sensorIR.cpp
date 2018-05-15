8a0e5362316af88df02e5ccd40232a18b8357fd7
Elonz03 <39248842+Elonz03@users.noreply.github.com>
2018-05-16 11:21:41 +1200

    Update sensorIR.cpp

--- sensorIR.cpp
+++ sensorIR.cpp
@@ -6,36 +6,52 @@
 int main(){
    init();
  
+ boolean inMaze = true;
  int adc_left;// left sensor - channel 0
  int adc_front; // front sensor - channel 1
- int adc_right; //right sensor - channel 7
+ int adc_right; //right sensor - channel 2
    
- for(int i=0; i<10; i++){
-   adc_left = read_analog(0);
-   adc_front = read_analog(1);
-   adc_right = read_analog(7);
+
+ 
+ while(inMaze){
+ adc_left = read_analog(0); //reads left IR value
+ adc_front = read_analog(1); //reads front IR value
+ adc_right = read_analog(2); //reads right IR value
+ 
+ //leftIR further from wall than rightIR. frontIR is close to wall. Turn left 
+ if(adc_left<adc_right && adc_front >= 300){  
+   turn_left();
+ }
+ 
+ //rightIR further from wall than leftIR. frontIR is close to wall. Turn right
+ else if(adc_right<adc_left && adc_front >= 300){ 
+   turn_right();
+ }
     
-   printf("%d\n", adc_left); 
-   sleep1(1,0);
-    
+ //rightIR, leftIR and frontIR have high readings. Turn around  
+ else if(){
+   turn_around();
+ }
+ else{
+ 
+ }
+ 
+ }
+
 }
-    for(int i=0; i<10; i++){
-   adc_left = read_analog(0);
-   adc_front = read_analog(1);
-   adc_right = read_analog(7);
-    
-   printf("%d\n", adc_front); 
-   sleep1(1,0);
-    
+
+void turn_left(int duration){ // Turns left
+sleep1(0, duration); 
 }
-    for(int i=0; i<10; i++){
-   adc_left = read_analog(0);
-   adc_front = read_analog(1);
-   adc_right = read_analog(7);
-    
-   printf("%d\n", left); 
-   sleep1(1,0);
-    
+
+void turn_right(int duration){ // Turns right
+sleep1(0, duration); 
+}
+
+void turn_around(int duration){ // Turns around 180
+sleep1(0, duration); 
 }
    
   }
+//300 front
+//400 sides
