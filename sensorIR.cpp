49f459c0af6176437ade5e70c3eb94d683bdfdd0
Elonz03 <39248842+Elonz03@users.noreply.github.com>
2018-05-16 10:31:12 +1200

    Update sensorIR.cpp

--- sensorIR.cpp
+++ sensorIR.cpp
@@ -6,8 +6,36 @@
 int main(){
    init();
  
- int adc_reading0;// left sensor
- int adc_reading1; // front sensor
- int adc_reading7; //right sensor
+ int adc_left;// left sensor - channel 0
+ int adc_front; // front sensor - channel 1
+ int adc_right; //right sensor - channel 7
+   
+ for(int i=0; i<10; i++){
+   adc_left = read_analog(0);
+   adc_front = read_analog(1);
+   adc_right = read_analog(7);
+    
+   printf("%d\n", adc_left); 
+   sleep1(1,0);
+    
+}
+    for(int i=0; i<10; i++){
+   adc_left = read_analog(0);
+   adc_front = read_analog(1);
+   adc_right = read_analog(7);
+    
+   printf("%d\n", adc_front); 
+   sleep1(1,0);
+    
+}
+    for(int i=0; i<10; i++){
+   adc_left = read_analog(0);
+   adc_front = read_analog(1);
+   adc_right = read_analog(7);
+    
+   printf("%d\n", left); 
+   sleep1(1,0);
+    
+}
    
   }
