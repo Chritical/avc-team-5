acbe2008b773abfb5852464c9d3e19463f8c235e
Elonz03 <39248842+Elonz03@users.noreply.github.com>
2018-05-16 10:17:08 +1200

    Create sensorIR.cpp

---
+++ sensorIR.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <time.h> 
+#include "E101.h"
+
+
+int main(){
+   init();
+ 
+ int adc_reading0;// left sensor
+ int adc_reading1; // front sensor
+ int adc_reading7; //right sensor
+   
+  }
