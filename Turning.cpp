c42dbd65f7b85eadaf0b757c9d7ea63567ab7da9
MulloFrank <38895960+MulloFrank@users.noreply.github.com>
2018-05-16 10:48:06 +1200

    Update Turning.cpp

--- Turning.cpp
+++ Turning.cpp
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include "E101.h"
-void TurnLeft(v_left, v_right);
-void TurnRight(v_left, v_right);
+void Turn(v_left, v_right);
 int main() {
 	int final v_go = 64;
 	int final Kp = 1;
@@ -21,14 +20,11 @@ int main() {
 		else if (v_right > 255){
 			v_right = 255;
 		}
-		if (error < 1000 && error > 0){
-			TurnLeft(v_left, v_right);
-		}
-		else if (error < 0 && error > -1000){
-			TurnRight(v_left, v_right);
+		if (error < 1000 && error > -1000){
+			Turn(v_left, v_right);
 		}
 		
-		else (error > 1000){
+		else if (error > 1000){
 			set_motor(1,v_go):
 			set_motor(2,v_go);	
 		}
@@ -38,12 +34,8 @@ int main() {
 return 0;
 }
 
-void TurnLeft(int v_left, int v_right) {
+void Turn(int v_left, int v_right) {
 	set_motor(1,v_right);
 	set_motor(2,v_left);
 }
 
-void TurnRight(int v_left, int v_right) {
-	set_motor(1,v_left);
-	set_motor(2,v_right);
-}
