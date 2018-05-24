3ddcbc4cad32c717a381c51155ff8b1f45ba89ab
mazzy999 <38895965+mazzy999@users.noreply.github.com>
2018-05-24 15:30:53 +1200

    Create getRightSensor.cpp

---
+++ getRightSensor.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+#include <time.h> 
+#include "E101.h"
+
+
+int main(){
+   init();
+ 
+ int adc_left;// left sensor - channel 0
+ int adc_front; // front sensor - channel 1
+ int adc_right; //right sensor - channel 2
+ 
+ while (true){
+      sleep1(2,0);
+      
+      adc_right = analog_read(2);
+      printf("%d", adc_right);
+      }
+ }
+   
